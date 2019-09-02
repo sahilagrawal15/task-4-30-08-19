c22c14ea07a3ab1f313d581fc50c0ba470b63e82
Sahil <sahilagrawal015@gmail.com>
2019-09-02 09:36:34 +0530

    Sorting using comparator(Part of task 5) (Asc and desc) in case 8

--- TestCollection/src/Main.java
+++ TestCollection/src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.Comparator;
+
 import java.util.Scanner;
 
 public class Main {
@@ -22,8 +24,7 @@ public class Main {
 		int a;
 		a = scan.nextInt();
 		switch (a) {
-		case 1:
-		{
+		case 1: {
 			System.out.println("Enter empno");
 			int empno = scan.nextInt();
 			System.out.println("Enter emp name");
@@ -39,8 +40,7 @@ public class Main {
 			stud.add(student);
 			break;
 		}
-		case 2:
-		{
+		case 2: {
 			System.out.println("Displaying record of all employees");
 			for (int i = 0; i < stud.size(); i++) {
 				// System.out.println(stud.toArray().toString());
@@ -48,8 +48,7 @@ public class Main {
 				break;
 			}
 		}
-		case 3:
-		{
+		case 3: {
 			System.out.println("Enter empno for removing its data");
 			int eno = scan.nextInt();
 			for (int j = 0; j < stud.size(); j++) {
@@ -60,16 +59,14 @@ public class Main {
 			}
 			break;
 		}
-		case 4:
-		{
+		case 4: {
 			System.out.println("Clearing all data");
 
 			stud.clear();
 			break;
 		}
 
-		case 5:
-		{
+		case 5: {
 			System.out.println("Enter emp no and change salary");
 			int em = scan.nextInt();
 			for (int p = 0; p < stud.size(); p++) {
@@ -81,8 +78,7 @@ public class Main {
 			break;
 		}
 
-		case 6:
-		{
+		case 6: {
 			System.out.println("Enter empno for searching");
 			int eno2 = scan.nextInt();
 			for (int k = 0; k < stud.size(); k++) {
@@ -91,8 +87,7 @@ public class Main {
 			}
 			break;
 		}
-		case 7:
-		{
+		case 7: {
 			String dnm = scan.next();
 			for (int t = 0; t < stud.size(); t++) {
 				if (stud.get(t).getDept().equals(dnm)) {
@@ -101,10 +96,185 @@ public class Main {
 			}
 			break;
 		}
+		case 8: {
+			System.out.println("Choose among the following to return data in sorted order");
+			System.out.println("1. Return data Emp name wise");
+			System.out.println("2. Return data Emp no wise");
+			System.out.println("3. Return data salary wise");
+			System.out.println("4. Return data Designation wise");
+			System.out.println("5. Return data dept wise");
+
+			int m = scan.nextInt();
+
+			switch (m) {
+			case 1: {
+				System.out.println("Select among the following");
+				System.out.println("1. Ascending");
+				System.out.println("2. Descending");
+				int j=scan.nextInt();
+				if(j==1)
+				{
+				Collections.sort(stud, new Comparator<Student>() {
+
+					@Override
+					public int compare(Student s1, Student s2) {
+
+						return s1.getEname().compareTo(s2.getEname());
+					}
+
+				});
+				}
+				else if(j==2)
+				{
+					Collections.sort(stud, new Comparator<Student>() {
+
+						@Override
+						public int compare(Student s1, Student s2) {
+
+							return s2.getEname().compareTo(s1.getEname());
+						}
+
+					});
+				}
+				break;
+			
+			}
+			case 2: {
+				System.out.println("Select among the following");
+				System.out.println("1. Ascending");
+				System.out.println("2. Descending");
+				int j=scan.nextInt();
+				if(j==1)
+				{
+				Collections.sort(stud, new Comparator<Student>() {
+
+					@Override
+					public int compare(Student s1, Student s2) {
+
+						return s1.getEmpno()-(s2.getEmpno());
+					}
+
+				});
+				}
+				else if(j==2)
+				{
+					Collections.sort(stud, new Comparator<Student>() {
+
+						@Override
+						public int compare(Student s1, Student s2) {
+
+							return s2.getEmpno()-(s1.getEmpno());
+					}
+				});
+				}
+				break;
+			}
+			case 3: {
+				System.out.println("Select among the following");
+				System.out.println("1. Ascending");
+				System.out.println("2. Descending");
+				int j=scan.nextInt();
+				if(j==1)
+				{
+			
+				Collections.sort(stud, new Comparator<Student>() {
+
+					@Override
+					public int compare(Student s1, Student s2) {
+
+						return s1.getSalary()-(s2.getSalary());
+					}
+
+				});
+				}
+				else if(j==2)
+				{
+					Collections.sort(stud, new Comparator<Student>() {
+
+						@Override
+						public int compare(Student s1, Student s2) {
+
+							return s2.getSalary()-(s1.getSalary());
+							}
+				});
+				}
+				break;
+			}
+			case 4: {
+				System.out.println("Select among the following");
+				System.out.println("1. Ascending");
+				System.out.println("2. Descending");
+				int j=scan.nextInt();
+				if(j==1)
+				{
+			
+				Collections.sort(stud, new Comparator<Student>() {
+
+					@Override
+					public int compare(Student s1, Student s2) {
+
+						return s1.getDesignation().compareTo(s2.getDesignation());
+					}
+
+				});
+				}
+				else if(j==2)
+				{
+					Collections.sort(stud, new Comparator<Student>() {
+
+						@Override
+						public int compare(Student s1, Student s2) {
+
+							return s2.getDesignation().compareTo(s1.getDesignation());
+							}
+				});
+				}
+				break;
+			}
+			case 5: {
+				System.out.println("Select among the following");
+				System.out.println("1. Ascending");
+				System.out.println("2. Descending");
+				int j=scan.nextInt();
+				if(j==1)
+				{
+			
+				Collections.sort(stud, new Comparator<Student>() {
+
+					@Override
+					public int compare(Student s1, Student s2) {
+
+						return s1.getDept().compareTo(s2.getDept());
+					}
+					});
+				}
+				
+
+				
+			
+			else if(j==2)
+			{
+				Collections.sort(stud, new Comparator<Student>() {
+
+					@Override
+					public int compare(Student s1, Student s2) {
+
+						return s1.getDept().compareTo(s2.getDept());
+			}
+				
+				});
+			}
+			break;
+			}
+			
+
+			}
+		}
+
 		default:
 			System.out.println("Exit");
 		}
 
-	}
-
+	
+		}
 }
